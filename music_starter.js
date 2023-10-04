153b60bc5a79ac17b4b96527c793044decb3915a
LilBliP <140349870+LilBliP@users.noreply.github.com>
2023-10-05 08:39:04 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -6,33 +6,65 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   ellipseMode(RADIUS)
   textSize(24);
   noStroke()
-  // let yellow = (247, 193, 12)
-  // let red = (204, 47, 12)
- 
-  let lerpmap = map(drum, 0, 100, 0, 1);
-  //let middleColour = lerpColor(red, yellow, lerpmap)
-  var linex = map(bass / 2 + other / 3, 0, 100, width / 2, width);
-  var linexmirror = map(bass / 2 + other / 3, 0, 100, width / 2, 0);
-  var explosion = map(drum, 0, 100, height / 2, 0);
-// fill(middleColour)
-rect(linex, height / 2, 20, height);
-rect(linexmirror, height / 2, 20, height);
-rect(width / 2, height, explosion, explosion);
 
-fill(100, 255, 100, 100);
-rect(0, 0, 720, 1280)
+ let vocalloud = map(vocal, 50, 100, 0, 100)
+  let linex = map(other, 0, 100, width / 2, width);
+  let liney = height
+  let otherthickness = map(other, 0, 100, 0, 20);
+  let linexmirror = map(other, 0, 100, width / 2, 0);
+let otheropacity = map(other, 0, 100, 0, 255);
+  let explosion = map(drum, 0, 100, 0, height / 2);
 
-}
-function draw_one_frame(words, vocal, drum, bass, other, counter){
+//rect(linexmirror, height / 2, 20, height);
 
-  
+//fill(100)
+//rect(width/2, height/2, explosion, explosion);
+
+
+
+// for(let i=1; i < 6; i++){
+//   let ellipseMove = i*20
+// ellipse(width/2+ellipseMove,height/2+ellipseMove,20,20)
+// }
+
+  push();
+  //https://www.youtube.com/watch?v=-MUOweQ6wac
 radialGradient(
   width/2, height/2, 0,
-  width/2, height/2, 500,
-  color(255, 255, 255, 0), 
-  color(255, 255, 100, 100), 
+  width/2, height/2, width+vocal*10,
+  color(255, 255, 100, 10), 
+  color(255, 255, 100, 150), 
 );
-ellipse(width/2, height/2, 400, 400);
+ellipse(width/2, height/2, width, height);
+pop();
+
+if(counter>1400){
+  fill(0)
+  push();
+//https://www.youtube.com/watch?v=-MUOweQ6wac
+radialGradient(
+width/2, height/2, vocal,
+width/2, height/2, vocal*5,
+color(10, 10, 10, 255),
+color(55, 55, 55, 0),  
+);
+ellipse(width/2, height/2, vocal*5, vocal*5);
+pop();
+}
+
+let rectHeight = map(drum, 0, 100, 300, 700);
+rectMode(CORNER)
+
+//for(let i=0; i<100;i++){
+  //push()
+  fill(10,10,10,otheropacity);
+  rect(linex, liney, otherthickness, -rectHeight);
+  rect(linex, liney, otherthickness, -height/3);
+  rect(linex, liney, otherthickness, -height/4);
+//pop()
+  liney = liney-10//-i}
+
+
 }
 
 
@@ -42,71 +74,7 @@ function radialGradient(sX, sY, sR, eX, eY, eR, colorS, colorE){
   );
   gradient.addColorStop(0, colorS);
   gradient.addColorStop(1, colorE);
-
   drawingContext.fillStyle = gradient;
 
-  
 }
 
-//let bar_spacing = height / 10;
-//  let bar_height = width / 12;
-//  let bar_pos_x = width / 2;
-
-
-//  // vocal bar is red
-//  fill(200, 0, 0);
-//  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-//  fill(0);
-//  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
-
-//  // drum bar is green
-//  fill(0, 200, 0);
-//  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-//  fill(0);
-//  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
-
-//  // bass bar is blue
-//  fill(50, 50, 240);
-//  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-//  fill(0);
-//  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
-
-//  // other bar is white
-//  fill(200, 200, 200);
-//  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-//  fill(0);
-//  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-//  fill(255, 255, 0);
-
-//  // display "words"
-//  textAlign(CENTER);
-//  textSize(vocal);
-//  text(words, width/2, height/3);
-
-
-
-// fill(other+100,vocal+50,25)
-//   rect(width/2+other*2,height/2,10,height)
-//   rect(width/2+other*3,height/2,10,height)
-//   rect(width/2+other*4,height/2,10,height)
-//   rect(width/2+other*5,height/2,10,height)
-//   rect(width/2-other*2,height/2,10,height)
-//   rect(width/2-other*3,height/2,10,height)
-//   rect(width/2-other*4,height/2,10,height)
-//   rect(width/2-other*5,height/2,10,height)
-
-
-//   fill(other,bass+vocal,other+bass)
-// rect(0,0,5*drum,5*drum)
-// rect(width,height,5*drum,5*drum)
-
-// fill(vocal+other,bass+vocal,other+bass)
-// ellipse(width/2,height/4,vocal,vocal)
-//   ellipse(width/2,height/4*2,vocal,vocal)
-// ellipse(width/2,height/4*3,vocal,vocal)
-
-//  //display "words"
-//     textAlign(CENTER);
-//     textSize(vocal);
-//    text(words, width/2, height/2.5);
-//    text(words, width/2, height/1.5);
