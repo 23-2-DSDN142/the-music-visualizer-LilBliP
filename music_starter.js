859c10a1d9d0324fa1fde90e78e7532b458c7ba1
LilBliP <140349870+LilBliP@users.noreply.github.com>
2023-10-16 11:30:34 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -3,8 +3,12 @@ let firstRun=true;
 let Torso,Arms,LegL,LegR;
 let titanX;
 let titanY;
+let sunSize;
+let sunOuterC;
+let sunInnerC;
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(200)
+  background(100)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   ellipseMode(RADIUS)
@@ -30,12 +34,27 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   let linexmirror = map(other, 0, 100, width / 2, 0);
 let otheropacity = map(other, 0, 100, 0, 255);
   let explosion = map(drum, 0, 100, width-200, width);
-  let titanY = map(counter,0,8000,height+80,height/2)+map(other,0,100,0,25)
+
+
+push()
+  if(counter>6000){
+    titanY = height/2+map(other,0,100,0,25)
+sunInnerC = vocal
+sunOuterC = vocal*5
+sunSize = vocal*8
+  }else{
+    titanY = map(counter,0,6000,height+80,height/2)+map(other,0,100,0,25)
+    sunInnerC = vocal
+    sunOuterC = vocal*8
+    sunSize = vocal*10
+  }
+pop()
 
 //rect(linexmirror, height / 2, 20, height);
 
 let titanX = width/2
 
+//7200 words begina again flares and shootimg time light reflect back revel features of titan gatcannon and plasma
 
 
 
@@ -52,12 +71,12 @@ if(counter>1400){
   push();
 //https://www.youtube.com/watch?v=-MUOweQ6wac
 radialGradient(
-width/2, height/2, vocal,
-width/2, height/2, vocal*5,
-color(10, 10, 10, 255),
-color(55, 55, 55, 0),  
+width/2, titanY, sunInnerC,
+width/2, titanY, sunOuterC,
+color(250, 225, 10, 255),
+color(200, 150, 100, 0),  
 );
-ellipse(width/2, height/2, vocal*5, vocal*5);
+ellipse(width/2, height/2, sunSize, sunSize);
 pop();
 }
 
@@ -94,7 +113,8 @@ rectMode(CORNER)
  
 //pop()
   // liney = liney-10//-i}
-
+  
+  ellipse(titanX,titanY,20,20)
 }
 
 
