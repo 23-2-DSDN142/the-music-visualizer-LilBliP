a9b6860b1bc9f729d56fb2b0355be236b03ae702
LilBliP <140349870+LilBliP@users.noreply.github.com>
2023-10-16 10:23:10 +1300

    16/10/23

--- music_starter.js
+++ music_starter.js
@@ -1,24 +1,44 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+let firstRun=true;
+let Torso,Arms,LegL,LegR;
+let titanX;
+let titanY;
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(200)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   ellipseMode(RADIUS)
   textSize(24);
-  noStroke()
+  noStroke();
+  
+  if(firstRun){
+    imageMode(CENTER);
+    //Torso = loadImage('titanTorso.png');
+    Torso = loadImage('titanTorso.png');
+    Arms = loadImage('titanArms.png');
+    LegL = loadImage('titanlegL.png');
+    LegR = loadImage('titanlegR.png');
+    firstRun=false;
+  }
+  
 
- let vocalloud = map(vocal, 50, 100, 0, 100)
+
+ let vocalloud = map(vocal, 50, 100, 0, 100);
   let linex = map(other, 0, 100, width / 2, width);
-  let liney = height
+  let liney = height;
   let otherthickness = map(other, 0, 100, 0, 20);
   let linexmirror = map(other, 0, 100, width / 2, 0);
 let otheropacity = map(other, 0, 100, 0, 255);
-  let explosion = map(drum, 0, 100, 0, height / 2);
+  let explosion = map(drum, 0, 100, width-200, width);
+  let titanY = map(counter,0,8000,height+80,height/2)+map(other,0,100,0,25)
 
 //rect(linexmirror, height / 2, 20, height);
 
-//fill(100)
-//rect(width/2, height/2, explosion, explosion);
+let titanX = width/2
+
+
+
+
 
 
 
@@ -26,17 +46,6 @@ let otheropacity = map(other, 0, 100, 0, 255);
 //   let ellipseMove = i*20
 // ellipse(width/2+ellipseMove,height/2+ellipseMove,20,20)
 // }
-
-  push();
-  //https://www.youtube.com/watch?v=-MUOweQ6wac
-radialGradient(
-  width/2, height/2, 0,
-  width/2, height/2, width+vocal*10,
-  color(255, 255, 100, 10), 
-  color(255, 255, 100, 150), 
-);
-ellipse(width/2, height/2, width, height);
-pop();
 
 if(counter>1400){
   fill(0)
@@ -52,18 +61,39 @@ ellipse(width/2, height/2, vocal*5, vocal*5);
 pop();
 }
 
+push();
+scale(1);
+  image(Torso,titanX,titanY);
+  image(Arms,titanX-10,titanY+145);
+  image(LegL,titanX-150,titanY+310);
+  image(LegR,titanX+165,titanY+340);
+  pop();
+
+
+fill(100,50,50);
+rect(width/2, height, width, explosion);
+
+  push();
+  //https://www.youtube.com/watch?v=-MUOweQ6wac
+radialGradient(
+  width/2, height/2, 0,
+  width/2, height/2, width+vocal*10,
+  color(255, 255, 100, 10), 
+  color(255, 255, 100, 150), 
+);
+ellipse(width/2, height/2, width, height);
+pop();
+
+
+
 let rectHeight = map(drum, 0, 100, 300, 700);
 rectMode(CORNER)
 
 //for(let i=0; i<100;i++){
   //push()
-  fill(10,10,10,otheropacity);
-  rect(linex, liney, otherthickness, -rectHeight);
-  rect(linex, liney, otherthickness, -height/3);
-  rect(linex, liney, otherthickness, -height/4);
+ 
 //pop()
-  liney = liney-10//-i}
-
+  // liney = liney-10//-i}
 
 }
 
